95bbd47ec395a0c66c534867902cd09368d639b6
Jh0n16 <57267877+Jh0n16@users.noreply.github.com>
2024-09-23 17:55:35 -0300

    feat: adicionando estilização para o formulário de /cadastro

--- src/pages/cadastro/index.jsx
+++ src/pages/cadastro/index.jsx
@@ -1,9 +1,40 @@
+import { useForm } from "react-hook-form";
+import { MdEmail, MdLock, MdPerson } from "react-icons/md";
+
+import { api } from "../../services/api";
+
 import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+import { Container, Column, TitleCadastro, SubtitleCadastro, Text, LoginText, LinkLogin} from "./styles";
 
 const Cadastro = () => {
+    const { control, handleSubmit, formState: { errors } } = useForm({
+        reValidateMode: 'onChange',
+        mode: 'onChange'
+    })
+
     return (
         <>
             <Header />
+            <Container>
+                <Column>
+                    
+                </Column>
+                <Column>
+                    <TitleCadastro>Comece agora grátis</TitleCadastro>
+                    <SubtitleCadastro>Crie sua conta e make the change._</SubtitleCadastro>
+                    <form onSubmit='' >
+                        <Input placeholder="Nome Completo" name="nomeCompleto" leftIcon={<MdPerson />} control={control} />
+                        <Input placeholder="E-mail" name="email" type="email" leftIcon={<MdEmail />} control={control} />
+                        <Input placeholder="Senha" name="password" type="password" leftIcon={<MdLock />} control={control} />
+                        <Button title="Cadastrar" type="submit" variant="second" />
+                    </form>
+                    <Text>Ao clicar em "criar minha conta grátis", declaro que aceito as Políticas de Privacidade e os Termos de Uso da DIO.</Text>
+                    <LoginText>Já tenho uma conta. <LinkLogin>Fazer login!</LinkLogin></LoginText>
+                </Column>
+            </Container>
         </>
     );
 }
--- src/pages/cadastro/styles.js
+++ src/pages/cadastro/styles.js
@@ -5,9 +5,51 @@ export const Container = styled.div`
     max-width: 80%;
     margin: 0 auto;
     margin-top: 120px;
-
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
+`
+
+export const Column = styled.div`
+    flex: 1;
+
+    & form, & p {
+        max-width: 300px;
+    }
+`
+
+export const TitleCadastro = styled.h2`
+    font-style: normal;
+    font-weight: 700;
+    font-size: 32px;
+    margin-bottom: 20px;
+`
+
+export const SubtitleCadastro = styled.p`
+    font-size: 16px;
+    font-weight: 400;
+    margin-bottom: 30px;
+    
+`
+
+export const Text = styled.p`
+    font-size: 18px;
+    font-weight: 400;
+    margin: 25px 0;
+`
+
+export const LoginText = styled.p`
+    font-size: 12px;
+    font-weight: 400;
+    margin-top: 25px;
+`
+export const LinkLogin = styled.a`
+    text-decoration: none;
+    color: #00FF00;
+
+    &:hover {
+        cursor: pointer;
+        color: #009900;
+    }
 `
